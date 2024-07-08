faa3db7c922d6e56ea5dd0df75b8ecb28cf3a65c
John Brittain <jsb1982@gmail.com>
2024-07-08 20:32:05 +0100

    Update tests

--- workflows/EPI/modules/EpiLine/.test.sh
+++ workflows/EPI/modules/EpiLine/.test.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 rm -rf results/out
-CONDA_SUBDIR=osx-64 snakemake --use-conda --cores 1 --configfile=config/.test.yaml _test
+snakemake --use-conda --cores 1 --configfile=config/.test.yaml _test
--- workflows/EPI/sources/EpiLine_Simulation/.test.sh
+++ workflows/EPI/sources/EpiLine_Simulation/.test.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 rm -rf results/out
-CONDA_SUBDIR=osx-64 snakemake --cores 1 --use-conda --configfile=config/.test.yaml _test
+snakemake --cores 1 --use-conda --configfile=config/.test.yaml _test
