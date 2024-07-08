7740093859f9b2b6fbe2a0efd5501eb14270907d
John Brittain <jsb1982@gmail.com>
2024-07-08 17:53:45 +0100

    R styler

--- workflows/EPI/modules/EpiLine/workflow/scripts/epiline.R
+++ workflows/EPI/modules/EpiLine/workflow/scripts/epiline.R
@@ -1,6 +1,6 @@
-library( EpiLine )
-library( plotly )
-library( optparse )
+library(EpiLine)
+library(plotly)
+library(optparse)
 
 ########################################################################################
 ### Command line arguments
@@ -8,24 +8,59 @@ library( optparse )
 
 # Parse the command line arguments
 option_list <- list(
-  make_option( c("-o", "--output"), type = "character", default = "epiline", help = "Output file stem" ),
-  make_option( c("-q", "--quantiles"), type = "character", default = "0.1, 0.5, 0.9", help = "Quantiles to plot" ),
-  make_option( c("-p", "--plot-delay"), type = "integer", default = 15, help = "Delay in seconds before taking a screenshot of the plot" ),
-  make_option( c("-d", "--date-of-report"), type = "character", default = NULL, help = "Date of the report" ),
-  make_option( c("-r", "--reported-file"), type = "character", help = "Filename of reported cases" ),
-  make_option( c("-l", "--linelist-file"), type = "character", help = "Filename of line list (report date, symptom date)" ),
-  make_option( c("--mcmc-samples"), type = "integer", default = 100, help = "Number of MCMC samples" ),
-  make_option( c("--mcmc-chains"), type = "integer", default = 1, help = "Number of MCMC chains" )
+  make_option(
+    c("-o", "--output"),
+    type = "character",
+    default = "epiline",
+    help = "Output file stem"
+  ),
+  make_option(
+    c("-q", "--quantiles"),
+    type = "character",
+    default = "0.1, 0.5, 0.9",
+    help = "Quantiles to plot"),
+  make_option(
+    c("-p", "--plot-delay"),
+    type = "integer",
+    default = 15,
+    help = "Delay in seconds before taking a screenshot of the plot"
+  ),
+  make_option(
+    c("-d", "--date-of-report"),
+    type = "character",
+    default = NULL,
+    help = "Date of the report"
+  ),
+  make_option(
+    c("-r", "--reported-file"),
+    type = "character",
+    help = "Filename of reported cases"
+  ),
+  make_option(
+    c("-l", "--linelist-file"),
+    type = "character",
+    help = "Filename of line list (report date, symptom date)"),
+  make_option(
+    c("--mcmc-samples"),
+    type = "integer",
+    default = 100,
+    help = "Number of MCMC samples"),
+  make_option(
+    c("--mcmc-chains"),
+    type = "integer",
+    default = 1,
+    help = "Number of MCMC chains"
+  )
 )
-opt_parser <- OptionParser( option_list = option_list )
-opt <- parse_args( opt_parser )
+opt_parser <- OptionParser(option_list = option_list)
+opt <- parse_args(opt_parser)
 
 # Transform and validate input arguments
-quantiles = as.numeric( strsplit( opt$quantiles, ",")[[1]] )  # "0.1, 0.2" -> [0.1, 0.2]
-if ( is.null( opt$"date-of-report" ) ) {
+quantiles <- as.numeric(strsplit(opt$quantiles, ",")[[1]]) # "0.1, 0.2" -> [0.1, 0.2]
+if (is.null(opt$"date-of-report")) {
   opt$"date-of-report" <- Sys.Date()
 }
-report_date <- as.Date( opt$"date-of-report" )
+report_date <- as.Date(opt$"date-of-report")
 
 ########################################################################################
 ### Model fitting
@@ -33,11 +68,11 @@ report_date <- as.Date( opt$"date-of-report" )
 
 # Fit the model
 fit <- symptom_report.fit(
-    file_reported = opt$"reported-file",
-    file_linelist = opt$"linelist-file",
-    report_date = report_date, 
-    mcmc_n_samples = opt$"mcmc-samples",
-    mcmc_n_chains = opt$"mcmc-chains"
+  file_reported = opt$"reported-file",
+  file_linelist = opt$"linelist-file",
+  report_date = report_date,
+  mcmc_n_samples = opt$"mcmc-samples",
+  mcmc_n_chains = opt$"mcmc-chains"
 )
 
 ########################################################################################
@@ -47,49 +82,52 @@ fit <- symptom_report.fit(
 # Symptoms plot
 p <- fit$plot.symptoms()
 html_file <- paste0(opt$output, "symptoms.html")
-htmlwidgets::saveWidget( p, html_file, selfcontained = TRUE )
+htmlwidgets::saveWidget(p, html_file, selfcontained = TRUE)
 webshot::webshot(
-    url = html_file,
-    file = paste0(opt$output, "symptoms.png"),
-    delay = opt$plot_delay
+  url = html_file,
+  file = paste0(opt$output, "symptoms.png"),
+  delay = opt$plot_delay
 )
 
 # Posterior distribution of daily growth rate r(t)
 p <- fit$plot.r()
-html_file <- paste0(opt$output, "r.html" )
-htmlwidgets::saveWidget( p, html_file, selfcontained = TRUE )
+html_file <- paste0(opt$output, "r.html")
+htmlwidgets::saveWidget(p, html_file, selfcontained = TRUE)
 webshot::webshot(
-    url = html_file,
-    file = paste0(opt$output, "r.png" ),
-    delay = opt$plot_delay
+  url = html_file,
+  file = paste0(opt$output, "r.png"),
+  delay = opt$plot_delay
 )
 
 # Symptom report distribution
 p <- fit$plot.symptom_report.dist()
-html_file <- paste0(opt$output, "symptom_report_dist.html" )
-htmlwidgets::saveWidget( p, html_file, selfcontained = TRUE )
+html_file <- paste0(opt$output, "symptom_report_dist.html")
+htmlwidgets::saveWidget(p, html_file, selfcontained = TRUE)
 webshot::webshot(
-    url = html_file,
-    file = paste0(opt$output, "symptom_report_dist.png" ),
-    delay = opt$plot_delay
+  url = html_file,
+  file = paste0(opt$output, "symptom_report_dist.png"),
+  delay = opt$plot_delay
 )
 
 # Symptom report quantiles
 for (attempt in 1:5) {
-    p <- fit$plot.symptom_report.quantiles( quantiles = quantiles )
-    htmlwidgets::saveWidget( p, html_file, selfcontained = TRUE )
-    html_file <- paste0(opt$output, "symptom_report_quantiles.html" )
-    tryCatch({
-        webshot::webshot(
-            url = html_file,
-            file = paste0(opt$output, "symptom_report_quantiles.png" ),
-            delay = opt$plot_delay
-        )
-    }, error = function(e) {
-        if (attempt == 5) {
-            stop("Failed to generate plot")
-        } else {
-            message(paste0("Failed to generate plot (attempt ", attempt, "), retrying..."))
-        }
-    })
+  p <- fit$plot.symptom_report.quantiles(quantiles = quantiles)
+  htmlwidgets::saveWidget(p, html_file, selfcontained = TRUE)
+  html_file <- paste0(opt$output, "symptom_report_quantiles.html")
+  tryCatch(
+    {
+      webshot::webshot(
+        url = html_file,
+        file = paste0(opt$output, "symptom_report_quantiles.png"),
+        delay = opt$plot_delay
+      )
+    },
+    error = function(e) {
+      if (attempt == 5) {
+        stop("Failed to generate plot")
+      } else {
+        message(paste0("Failed to generate plot (attempt ", attempt, "), retrying..."))
+      }
+    }
+  )
 }
--- workflows/EPI/sources/EpiLine_Simulation/workflow/scripts/epiline_sim.R
+++ workflows/EPI/sources/EpiLine_Simulation/workflow/scripts/epiline_sim.R
@@ -1,5 +1,5 @@
-library( EpiLine )
-library( optparse )
+library(EpiLine)
+library(optparse)
 
 ########################################################################################
 ### Command line arguments
@@ -7,41 +7,71 @@ library( optparse )
 
 # Parse the command line arguments
 option_list <- list(
-  make_option( c("-o", "--output"), type = "character", default = "epiline", help = "Output filename" ),
-  make_option( c("--start-date"), type = "character", default = "2020-04-01", help = "Start date of reporting" ),
-  make_option( c("--duration"), type = "integer", default = 50, help = "Number of days for which data is reported" ),
-  make_option( c("--pre-symptom"), type = "integer", default = 30, help = "Days before reporting period to simulate" ),
-  make_option( c("--post-symptom"), type = "integer", default = 5, help = "Days after reporting period to simulate" ),
-  make_option( c("--symptomatic"), type = "integer", default = 2, help = "Initial number of symptomatic individuals" )
+  make_option(
+    c("-o", "--output"),
+    type = "character",
+    default = "epiline",
+    help = "Output filename"
+  ),
+  make_option(
+    c("--start-date"),
+    type = "character",
+    default = "2020-04-01",
+    help = "Start date of reporting"
+  ),
+  make_option(
+    c("--duration"),
+    type = "integer",
+    default = 50,
+    help = "Number of days for which data is reported"
+  ),
+  make_option(
+    c("--pre-symptom"),
+    type = "integer",
+    default = 30,
+    help = "Days before reporting period to simulate"
+  ),
+  make_option(
+    c("--post-symptom"), 
+    type = "integer",
+    default = 5, 
+    help = "Days after reporting period to simulate"
+  ),
+  make_option(
+    c("--symptomatic"),
+    type = "integer",
+    default = 2,
+    help = "Initial number of symptomatic individuals"
+  )
 )
-opt_parser <- OptionParser( option_list = option_list )
-opt <- parse_args( opt_parser )
+opt_parser <- OptionParser(option_list = option_list)
+opt <- parse_args(opt_parser)
 
 ########################################################################################
 ### Simulation
 ########################################################################################
 
-set.seed( 1 )
+set.seed(1)
 
 # define the length of the simulation
-t_rep          <- opt$duration # number of days for which data is reported
-t_symptom_pre  <- opt$"pre-symptom"
+t_rep <- opt$duration # number of days for which data is reported
+t_symptom_pre <- opt$"pre-symptom"
 t_symptom_post <- opt$"post-symptom"
-t_max          <- t_rep + t_symptom_post + t_symptom_pre
+t_max <- t_rep + t_symptom_post + t_symptom_pre
 
 # set up the variable r(t) and distribution
-symptom_0 <- opt$symptomatic                  # initial number of symptomatic individuals
-r         <- 0.1 - 0.13 * ( 1:t_max ) / t_max # r(t) in the simulation
-xi        <- -1 + 6 * ( t_max:1 ) / t_max     # xi parameter in the symptom-report dist
-lambda    <- 2 + ( t_max:1 ) / t_max          # lambda parameter in the symptom-report dist
+symptom_0 <- opt$symptomatic # initial number of symptomatic individuals
+r <- 0.1 - 0.13 * (1:t_max) / t_max # r(t) in the simulation
+xi <- -1 + 6 * (t_max:1) / t_max # xi parameter in the symptom-report dist
+lambda <- 2 + (t_max:1) / t_max # lambda parameter in the symptom-report dist
 
 simulation <- symptom_report.simulator(
-  t_rep          = t_rep,
-  t_symptom_pre  = t_symptom_pre,
+  t_rep = t_rep,
+  t_symptom_pre = t_symptom_pre,
   t_symptom_post = t_symptom_post,
-  symptom_0   = symptom_0,
-  r           = r,
-  dist_xi     = xi,
+  symptom_0 = symptom_0,
+  r = r,
+  dist_xi = xi,
   dist_lambda = lambda
 )
 
@@ -50,18 +80,18 @@ simulation <- symptom_report.simulator(
 ########################################################################################
 
 # provide a date vector for the simulation
-start_date <- as.Date( "2020-04-02" )
+start_date <- as.Date("2020-04-02")
 date <- seq.Date(
   from = start_date,
-  by   = "day",
-  length.out = length( simulation$reported )
+  by = "day",
+  length.out = length(simulation$reported)
 )
 
 # convert the linelist to dates (see https://github.com/BDI-pathogens/Epiline)
-ll_report <- date[ simulation$linelist$report ]
+ll_report <- date[simulation$linelist$report]
 
 # offset date by days in 'symptom' (negative values relative to start of simulation)
-start_date_vector <- start_date + rep( 0, length( simulation$linelist$symptom ) )
+start_date_vector <- start_date + rep(0, length(simulation$linelist$symptom))
 ll_symptom <- start_date_vector + simulation$linelist$symptom
 
 
@@ -71,22 +101,22 @@ ll_symptom <- start_date_vector + simulation$linelist$symptom
 
 # reported.csv
 data <- data.frame(
-    date = date,
-    reported = simulation$reported
+  date = date,
+  reported = simulation$reported
 )
 write.csv(
-    data,
-    file = paste0(opt$output, "reported.csv"),
-    row.names = FALSE
+  data,
+  file = paste0(opt$output, "reported.csv"),
+  row.names = FALSE
 )
 
 # linelist.csv
 data <- data.frame(
-    report = ll_report,
-    symptom = ll_symptom
+  report = ll_report,
+  symptom = ll_symptom
 )
 write.csv(
-    data,
-    file = paste0(opt$output, "linelist.csv"),
-    row.names = FALSE
+  data,
+  file = paste0(opt$output, "linelist.csv"),
+  row.names = FALSE
 )
