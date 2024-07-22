ecf014d75eae1940eb1b642774c2977d4e4c82b1
John Brittain <jsb1982@gmail.com>
2024-07-22 14:40:33 +0100

    Update Download URl; skip (interactive module) Plot tests

--- workflows/TutorialBuilder/modules/Plot/.test.sh
+++ workflows/TutorialBuilder/modules/Plot/.test.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
+# !!! This module is not currently tested as it requires user interaction !!!
+
 rm -rf results/out
-snakemake --use-conda --cores 1 --configfile=config/.test.yaml _test
+# snakemake --use-conda --cores 1 --configfile=config/.test.yaml _test
