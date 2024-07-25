89237709db4eb3c683939b0b64761eca4ff31e57
John Brittain <jsb1982@gmail.com>
2024-07-25 10:43:44 +0100

    (Sampler_Brito) Modify for use as a module

--- workflows/EPI/modules/Subsampler_Brito/.test.sh
+++ workflows/EPI/modules/Subsampler_Brito/.test.sh
@@ -1,4 +1,18 @@
 #!/usr/bin/env bash
 
+# Test as workflow
 rm -rf results/out
-snakemake --use-conda --cores 1 --configfile=config/.test.yaml _test
+snakemake \
+    --cores 1 \
+    --use-conda \
+    --configfile=config/.test.yaml \
+    _test
+
+# Test as module
+rm -rf results/out
+snakemake \
+    --cores 1 \
+    --use-conda \
+    --snakefile workflow/.Snakefile.test.module \
+    --configfile=config/.test.module.yaml \
+    subsampler_brito__test
