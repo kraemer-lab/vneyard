bc8395bbed4ee6d5d0bb2a2014e7770f6d278bf4
jsbrittain <98161205+jsbrittain@users.noreply.github.com>
2024-09-18 15:28:47 +0100

    Update Download to use grapevne_helper (#48)
    * Update Download to use grapevne_helper
    
    * Update pull checks to snakemake 8

--- workflows/TutorialBuilder/sources/Download/.test.sh
+++ workflows/TutorialBuilder/sources/Download/.test.sh
@@ -2,6 +2,4 @@
 
 rm -rf results/out
 
-# !!! AUTO.remote is demonstrating strange behaviour on the github runner; needs investigating
-
-# snakemake --use-conda --cores 1 --configfile=config/.test.yaml _test
+snakemake --use-conda --cores 1 --configfile=config/.test.yaml _test
